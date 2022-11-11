68cf95961353e84b461c95d79de8a364b4498951
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-11-11 13:34:29 +0000

    create a BabyElement 'cookie cutter' function

---
+++ src/components/BabyElement.tsx
@@ -0,0 +1,14 @@
+import { babyNamesProps } from "../baby-names";
+
+interface babyNamesMAPprops {
+    name: babyNamesProps
+}
+
+export function oneBabyElement(props: babyNamesMAPprops): JSX.Element {
+    const name = props.name
+    return (
+        <>
+            {name.sex === 'm' ? <p className="male">{name.name}</p>:<p className="female">{name.name}</p>}
+        </>
+    )
+}
